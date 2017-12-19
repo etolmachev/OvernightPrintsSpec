fd7e412b0fb55c9659c8a43d3a1e16c5a7a12aab
DonkeyRit <alekseevda24@gmail.com>
2017-12-19 12:22:07 +0400

    123

---
+++ OvernightPrintsSpecBindings/Bindings/ResetPasswordBindings.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OvernightPrintsSpecBindings.TestBase;
+using OvernightPrintsSpecBindings.TestBase.Pages;
+using TechTalk.SpecFlow;
+
+namespace OvernightPrintsSpecBindings.Bindings
+{   
+    [Binding]
+    class ResetPasswordBindings
+    {
+        ResetPasswordPopUpPage _resetPasswordPopUp = new ResetPasswordPopUpPage();
+
+        [Then(@"I see Reset Password popup")]
+        public void ThenISeeResetPasswordPopup()
+        {
+            Assert.AreEqual("RESET PASSWORD", _resetPasswordPopUp.GetLabelText());
+        }
+
+        [When(@"I set following parameters on Reset Password popup")]
+        public void WhenISetFollowingParametersOnResetPasswordPopup(Table table)
+        {
+            foreach (var row in table.Rows)
+            {
+                string key = row["Field"];
+
+                switch (key)
+                {
+                    case "Email Address":
+                        _resetPasswordPopUp.TypeEmail(row["Value"]);
+                        break;
+
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        [When(@"I click Reset Password Button on Reset Password popup")]
+        public void WhenIClickResetPasswordButtonOnResetPasswordPopup()
+        {
+            _resetPasswordPopUp.ClickResetPassword();
+        }
+
+        [Then(@"I see notification message about successful reset password")]
+        public void ThenISeeNotificationMessageAboutSuccessfulResetPassword()
+        {
+            HtmlElement labelElement = new HtmlElement(By.CssSelector("#body-main > div > div > div.myonp.myonp-container.myonp-container-reset.header-align > h4"));
+            Assert.AreEqual("An email has been sent. It contains a link you must click to reset your password.", labelElement.Text);
+        }
+
+        [When(@"I click Cancel Button on Reset Password popup")]
+        public void WhenIClickCancelButtonOnResetPasswordPopup()
+        {
+            _resetPasswordPopUp.ClickCancel();
+        }
+
+
+    }
+}
---
+++ OvernightPrintsSpecBindings/TestBase/Pages/ResetPasswordPopUpPage.cs
@@ -0,0 +1,47 @@
+﻿    using System;
+using OpenQA.Selenium;
+
+namespace OvernightPrintsSpecBindings.TestBase.Pages
+{
+    public class ResetPasswordPopUpPage
+    {
+        private By _emailLocator = By.Id("username");
+        private By _buttonResetPasswordLocator = By.CssSelector("[value=\"Reset Password\"]");
+        private By _linkCancelLocator = By.CssSelector("#myonp-reset-pwd > form > div:nth-child(2) > div > a");
+        private By _labelLocator = By.CssSelector("#ui-id-3 > div > div > h1");
+
+        private HtmlElement EmailElement;
+        private HtmlElement ButtonResetPasswordElement;
+        private HtmlElement LinkCancelElement;
+        private HtmlElement LabelElement;
+
+        public ResetPasswordPopUpPage()
+        {
+            EmailElement = new HtmlElement(_emailLocator);
+            ButtonResetPasswordElement = new HtmlElement(_buttonResetPasswordLocator);
+            LinkCancelElement = new HtmlElement(_linkCancelLocator);
+            LabelElement = new HtmlElement(_labelLocator);
+        }
+
+        public void TypeEmail(String email)
+		{
+			EmailElement.SendKeys(email);
+			Console.WriteLine(EmailElement.GetAttribute("validity"));
+		}
+
+        public void ClickResetPassword()
+        {
+            ButtonResetPasswordElement.Click();
+        }
+
+        public void ClickCancel()
+        {
+            LinkCancelElement.Click();
+        }
+
+        public string GetLabelText()
+        {
+            return LabelElement.Text;
+        }
+    }
+}
