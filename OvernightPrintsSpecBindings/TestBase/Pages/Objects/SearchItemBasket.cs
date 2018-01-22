dfda2ec313c2bc30e1d85750bcbe9934b48ac891
DonkeyRit <alekseevda24@gmail.com>
2018-01-22 15:42:59 +0400

    Created an object for storing search results

---
+++ OvernightPrintsSpecBindings/TestBase/Pages/Objects/SearchItemBasket.cs
@@ -0,0 +1,89 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace OvernightPrintsSpecBindings.TestBase.Pages.Objects
+{
+	public class SearchItemBasket
+	{
+
+		private string productName;
+		private string quantityPrice;
+		private string materialPrice;
+		private string itemSubtotal;
+
+		private readonly string _itemExpectedEntityLocator = "//div[div/div/h2/span[contains(text(), '%s')]]";
+		private static readonly By _itemActuallyEntityLocator = By.ClassName("printeditem");
+		private static readonly By _headerItemLocator = By.ClassName("item-title");
+		private static readonly By _quantityPriceItemLocator = By.ClassName("item-sku-price");
+		private static readonly By _materialPriceItemLocator = By.CssSelector("ul > li > div > div.product-choice-price");
+		private static readonly By _itemSubtotalItemLocator = By.ClassName("sale-price");
+
+		public SearchItemBasket(TableRow row)
+		{
+			this.productName = row["Name"];
+			this.quantityPrice = row["Quantity Price"];
+			this.materialPrice = row["Material Price"];
+			this.itemSubtotal = row["Item Subtotal"];
+		}
+
+		public SearchItemBasket(HtmlElement entity)
+		{
+			this.productName = entity.FindElement(_headerItemLocator).Text;
+			this.quantityPrice = entity.FindElement(_quantityPriceItemLocator).Text;
+			this.materialPrice = entity.FindElement(_materialPriceItemLocator).Text;
+			this.itemSubtotal = entity.FindElement(_itemSubtotalItemLocator).Text;
+		}
+
+		public SearchItemBasket()
+		{
+		}
+
+		public static List<SearchItemBasket> GetAllItemsCart()
+		{
+			List<SearchItemBasket> items = new List<SearchItemBasket>();
+
+			ReadOnlyCollection<IWebElement> entities = Browser.Driver.FindElements(_itemActuallyEntityLocator);
+			foreach (var entity in entities)
+			{
+				SearchItemBasket item = new SearchItemBasket();
+				item.productName = entity.FindElement(_headerItemLocator).Text;
+				item.quantityPrice = entity.FindElement(_quantityPriceItemLocator).Text;
+				item.materialPrice = entity.FindElement(_materialPriceItemLocator).Text;
+				item.itemSubtotal = entity.FindElement(_itemSubtotalItemLocator).Text;
+
+				items.Add(item);
+			}
+
+			return items;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null || this.GetType() != obj.GetType())
+				return false;
+
+			SearchItemBasket item = (SearchItemBasket) obj;
+
+			if (this.productName.Equals("*") || this.productName.Contains(item.productName))
+			{
+				if (this.quantityPrice.Equals("*") || this.quantityPrice.Equals(item.quantityPrice))
+				{
+					if (this.materialPrice.Equals("*") || this.materialPrice.Equals(item.materialPrice))
+					{
+						if (this.itemSubtotal.Equals("*") || this.itemSubtotal.Equals(item.itemSubtotal))
+						{
+							return true;
+						}
+					}
+				}
+			}
+
+			return false;
+		}
+	}
+}
