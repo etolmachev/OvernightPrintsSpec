ba43b4c495f39649ba43d36c180297a30b9be80a
DonkeyRit <alekseevda24@gmail.com>
2018-01-30 16:03:40 +0400

    Changed base class Config

---
+++ OvernightPrintsSpecBindings/TestBase/Config/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace OvernightPrintsSpecBindings.TestBase.Config
+{
+	public class Configuration
+	{
+		private Dictionary<string,string> parameters = new Dictionary<string, string>();
+
+		public string GetParameter(string key)
+		{
+			return parameters[key];
+		}
+
+		public void AddParameter(string key, string value)
+		{
+			parameters.Add(key,value);
+		}
+
+	}
+}
